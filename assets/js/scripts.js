b64be60f47717a231df619adfd0ed449d0c44ab0
patrikpcm <patrikpcm@hotmail.com>
2024-07-09 11:49:11 -0300

    Ajuste de carousel da área de Certificações

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -23,3 +23,47 @@ $(function(){
         });
     });
 });
+
+/*
+CAROUSEL COM MÚLTIPLOS ITENS
+*/
+//To get the entire width of the carousel, use scrollWidth.
+var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+//To get the width of a single card in the carousel, use width().
+var cardWidth = $(".carousel-item").width();
+//Now, let’s use a variable to store the current scroll position which will initially be 0.
+var scrollPosition = 0;
+//When the next button is clicked, check if you’ve reached the 7th card, as you don’t want to scroll any further.
+$(".carousel-control-next").on("click", function () {
+    if (scrollPosition < ((cardWidth * 4) - carouselWidth)) { //check if you can go any further
+        scrollPosition += cardWidth;  //update scroll position
+        $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+    }
+});
+
+//Similarly, when the previous button is clicked, check if you are on the first card.
+$(".carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+        scrollPosition -= cardWidth;
+        console.log(scrollPosition);
+        $(".carousel-inner").animate({ scrollLeft: scrollPosition },600);
+    }
+});
+
+//parar o ciclo contínuo
+var multipleCardCarousel = document.querySelector(
+    "#carouselExampleControls"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+    //rest of the code
+    var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+      interval: false
+    });
+} else {
+    $(multipleCardCarousel).addClass("slide");
+}
+//parar o ciclo contínuo bootstrap 5.2+
+var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+    wrap: false,
+});
